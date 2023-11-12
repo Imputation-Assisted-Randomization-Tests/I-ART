53d9b5406ad1acccc597b2ad4f67eb0284c047be
jiawei-zhang-a <jz4721@nyu.edu>
2023-11-12 00:37:03 -0500

    Bug fix, solve the datatype self-modified problems, all converted to data.frame to ensure the type is correct

--- tests/test.R
+++ tests/test.R
@@ -3,13 +3,14 @@ devtools::install_github("Imputation-Assisted-Randomization-Tests/iArt", force =
 library(iArt)
 Z <- c(1, 1, 1, 1, 0, 0, 0, 0)
 X <- matrix(c(5.1, 3.5, 4.9, NA, 4.7, 3.2, 4.5, NA, 7.2, 2.3, 8.6, 3.1, 6.0, 3.6, 8.4, 3.9), ncol = 2)
-Y <- matrix(c(4.4, 0.5, 4.3, 0.7, 4.1, NA, 5.0, 0.4, 1.7, 0.1, NA, 0.2, 1.4, NA, 1.7, 0.4), ncol = 2)
-result <- iArt.test(Z = Z, X = X, Y = Y, L = 1000, verbose = TRUE)
+Y <- matrix(c(4.4, 4.3, 4.1, 5.0, 1.7, NA, 1.4, 1.7,0.5, 0.7, NA, 0.4, 0.1, 0.2, NA, 0.4), ncol = 2, byrow = TRUE)
+result <- iArt.test(Z = Z, X = X, Y = Y, L = 100, verbose = TRUE)
+
 print(result)
 
 Z <- c(1, 1, 1, 1, 0, 0, 0, 0)
 X <- matrix(c(5.1, 3.5, 4.9, 4.0, 4.7, 3.2, 4.5, 5, 7.2, 2.3, 8.6, 3.1, 6.0, 3.6, 8.4, 3.9), ncol = 2)
-Y <- matrix(c(4.4, 4.3, NA, 5.0, 1.7, NA, 1.4, 1.7), ncol = 1, byrow = TRUE)
+Y <- matrix(c(4.4, 4.3, NA, 5.0, 1.1, NA, 1.4, 1.7), ncol = 1, byrow = TRUE)
 result <- iArt.test(Z = Z, X = X, Y = Y, L = 100,covariate_adjustment = TRUE, verbose = TRUE)
 
 print(result)
